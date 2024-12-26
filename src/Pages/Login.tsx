e0c44930f22989d2ae706876475c56a66ef1abee
kazirauf <raufelahi9876@gmail.com>
2024-12-26 18:35:55 +0600

    add the Demo Admin & User Credential in login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -6,6 +6,7 @@ import toast from "react-hot-toast";
 import { useAppDispatch, useAppSelector } from "../Redux/hooks";
 import { selectCurrentUser, setUser } from "../Redux/Features/Auth/authSlice";
 import "./sign.css";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -38,72 +39,85 @@ const Login = () => {
   if (user) {
     return <Navigate to={"/"} />;
   }
-  const openModal = () => {
-    const modal = document.getElementById('my_modal_5') as HTMLDialogElement | null;
-    modal?.showModal();
+
+  // Handlers to set demo credentials
+  const setDemoUser = () => {
+    setEmail("user12@gmail.com");
+    setPassword("123");
+  };
+
+  const setDemoAdmin = () => {
+    setEmail("kllu@mallu.com");
+    setPassword("123");
   };
 
   return (
     <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form onSubmit={handleSubmit} action="#" className="sign-in-form">
-          <p onClick={openModal}>
-        Demo Admin & User Credential 
-          <span className="text-blue-500 font-bold text-lg underline ml-2">View</span>
-      </p>
-      
-      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello User</h3>
-        <div className="my-5">
-        <p className="">Admin Email Address <span className="text-blue-500">kllu@mallu.com</span></p>
-          <p className="">Admin Password <span className="text-blue-500">123</span></p>
-          <p className="">Admin Email Address <span className="text-blue-500">user12@gmail.com</span></p>
-          <p className="">Admin Password <span className="text-blue-500">123</span></p>
-        </div>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* If there is a button in the form, it will close the modal */}
-              <button className="btn">Close</button>
+      <div className="flex justify-center">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form onSubmit={handleSubmit} action="#" className="sign-in-form">
+              <p>
+                Demo Admin & User Credential
+                <span className="text-blue-500 font-bold text-lg ml-2">
+                  Click Now
+                </span>
+              </p>
+              <div className="flex gap-5 mb-10">
+                <button
+                  type="button"
+                  onClick={setDemoUser}
+                  className="py-2 px-5 shadow-lg rounded bg-blue-400 hover:bg-blue-600 text-white"
+                >
+                  <h1 className="text-xl font-bold mb-3">User</h1>
+                  <div className="text-start">
+                    <p className="text-base">Email: user12@gmail.com</p>
+                    <p className="text-base">Pass: 123</p>
+                  </div>
+                </button>
+                <button
+                  type="button"
+                  onClick={setDemoAdmin}
+                  className="py-2 px-5 shadow-lg rounded bg-blue-400 hover:bg-blue-600 text-white"
+                >
+                  <h1 className="text-xl font-bold mb-3">Admin</h1>
+                  <div className="text-start">
+                    <p className="text-base">Email: kllu@mallu.com</p>
+                    <p className="text-base">Pass: 123</p>
+                  </div>
+                </button>
+              </div>
+
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="Your email address"
+                />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  placeholder="Your password"
+                />
+              </div>
+              <button
+                disabled={isLoading}
+                type="submit"
+                className="mt-5 px-20 py-2 tracking-wider bg-black text-white rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black font-bold transition-colors duration-300 transform"
+              >
+                {isLoading ? "Loading..." : "Login"}
+              </button>
             </form>
           </div>
-        </div>
-      </dialog>
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                placeholder="Your email address"
-              />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input  type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                placeholder="Your password"/>
-            </div>
-            <button
-                  disabled={isLoading}
-                  type="submit"
-                  className="mt-5  px-20 py-2 tracking-wider  bg-black text-white rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-blackfont-bold transition-colors duration-300 transform"
-                >
-                  {isLoading ? (
-                    <button    className="mt-5  px-20 py-2 tracking-wider  bg-black text-white rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-blackfont-bold transition-colors duration-300 transform">Loading...</button>
-                  ) : (
-                    "Login"
-                  )}
-                </button>
-          
-        
-          </form>
-         
         </div>
       </div>
 
@@ -111,17 +125,17 @@ const Login = () => {
         <div className="panel left-panel">
           <div className="content">
             <h3>New here?</h3>
-            <p  >
-              
-            Already have an account? Sign in to log into your account and join us!
+            <p>
+              Already have an account? Sign in to log into your account and join
+              us!
             </p>
             <Link
-                  to={"/registration"}
-                 className="btn transparent" id="sign-up-btn"
-                >
-                  sign up
-                </Link>
-         
+              to={"/registration"}
+              className="btn transparent"
+              id="sign-up-btn"
+            >
+              sign up
+            </Link>
           </div>
           <img
             src="https://raw.githubusercontent.com/sefyudem/Sliding-Sign-In-Sign-Up-Form/955c6482aeeb2f0e77c1f3c66354da3bc4d7a72d/img/log.svg"
@@ -129,10 +143,8 @@ const Login = () => {
             alt=""
           />
         </div>
-       
       </div>
     </div>
-  
   );
 };
 
